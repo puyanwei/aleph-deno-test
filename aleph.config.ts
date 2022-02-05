b46f79a2ad98d0060e34c0c1dd6575db00b7c0f4
Puyan Wei <puyanwei@gmail.com>
2022-02-05 16:16:47 +0000

    Remove aleph config

--- aleph.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'https://deno.land/x/aleph/types.d.ts';
-
-export default <Config>{
-  build: {
-    target: 'es2015',
-    browsers: { chrome: 90, safari: 12 },
-    outputDir: '/dist',
-  },
-};
